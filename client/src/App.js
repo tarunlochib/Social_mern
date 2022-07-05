caa434b708c288f40d93f47e572a1ac269b09e36
Tarun Lochib <tarunlochib99@gmail.com>
2022-07-05 17:59:45 +0530

    post and form components

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
 import memories from "./Images/memories.png";
 
+import Posts from "./components/Posts/Posts.js";
+import Form from "./components/Form/Form.js";
+
 const App = () => {
     return (
      <Container maxWidth="lg">
@@ -11,8 +14,21 @@ const App = () => {
             <Typography variant="h2" align="center">
                 Portraits
             </Typography>
-            <img src={memories} alt="portraits" height="60" />
+            <img src={memories} alt="portraits" height="100" />
         </AppBar>
+
+        <Grow in>
+            <Container>
+                <Grid container justify="space-between" alignItems="strech" spacing={3}>
+                    <Grid item xs={12} sm={7}>
+                        <Posts />
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Form />
+                    </Grid>
+                </Grid>
+            </Container>
+        </Grow>
 
      </Container>
     )
